50a60b4b3b4b76bf501c8dadbbd6a275e77a9523
MINDSET-SUDO <alexmuigai85@gmail.com>
2023-10-25 12:05:21 +0300

    initial commit

---
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        setContentView(R.layout.activity_main);
+
+        // Display a Toast message
+        Toast.makeText(this, "Hello, World!", Toast.LENGTH_SHORT).show();
+    }
+}
+
